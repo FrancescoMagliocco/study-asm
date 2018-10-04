41a11650db4ff5ade4efd09f1f3374e977eb3e80
FrancescoMagliocco <Magliocco.W.Francesco@gmail.com>
2018-10-04 19:28:16 -0400

    Removed usless lines

--- read.asm
+++ read.asm
@@ -19,63 +19,50 @@ _start:
 	int	0x80
 
 write:
-	push	ebp
 	push	eax
 	push	ebx
-
-	mov	ebp,	esp
 
 	mov	eax,	4	; sys_write
 	mov	ebx,	1	; stdout
 
 	int	0x80
 
-	mov	esp,	ebp
-
 	pop	ebx
 	pop	eax
-	pop	ebp
 	ret
 
 read:
-	push	ebp
 	push	eax
 	push	ebx
 	push	ecx
 	push	edx
 
-	mov	ebp,	esp
-
 loopa:
 	mov	eax,	3	; sys_read
 	mov	ebx,	0	; stdin
 	mov	ecx,	buf	; buffer
-	mov	edx,	4	; max length
+	mov	edx,	100	; max length
 
 	int	0x80
 
 ;	cmp	eax,	0	; end loop if read <= 0
 ;	jle	loopa
 
-	push	ecx
 	push	edx
 
 	mov	ecx,	0x0a
 	mov	edx,	1
 	call	write
 
-	pop	edx
 	pop	ecx
 
 	mov	ecx,	buf	; buffer
 	mov	edx,	eax	; length
 	call	write
 
-	mov	esp,	ebp
 	pop	edx
 	pop	ecx
 	pop	ebx
 	pop	eax
-	pop	ebp
 ;	call	write
 	ret
